34a8161443e264e1ef8797628cc035116d9fa8b3
hamza19mughal99 <hamza19mughal99@gmail.com>
2022-01-19 11:54:54 +0500

    new-files

---
+++ src/container/CarDetails/CarDetails.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import "./CarDetails.css";
+
+const CarDetails = () => {
+    return (
+        <div className='car_details_main'>
+            <Container>
+                <Row className={'justify-content-center'}>
+                    <Col md={3} className={'car_title'}>
+                        <h5>Car.title</h5>
+                        <hr />
+                        <p> Price: <span> car.price </span> </p>
+                        <p> Created Date: <span> car.create_at</span></p>
+                    </Col>
+                    <Col md={6} className={'car_title'}>
+                        <div className={'d-flex justify-content-around'}>
+                            <div>
+                                <p>Make: <span> car.make </span></p>
+                                <p>Model: <span> car.model </span></p>
+                                <p>Year: <span> car.year </span></p>
+                                <p>Price: <span> car.price </span></p>
+                                <p>Transmission: <span> car.transmission </span></p>
+                                <p>FuelType: <span> car.fueltype </span></p>
+                            </div>
+                            <div>
+                                <p>Description: <span>car.discription</span></p>
+                                <p>Features: <span>car.feature</span></p>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={2} className={'car_title'}>
+                        <p>Viewed This: <span>car.visitcount</span></p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+export default CarDetails;
---
+++ src/container/ManageAds/ManageAds.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import "./ManageAds.css";
+
+const ManageAds = () => {
+    return (
+        <React.Fragment>
+            <div className="main_manage_ads">
+                <Container>
+                    <Row>
+                        <Col md={3} className='car_details'>
+                            <p>Title: <span> car.title </span> </p>
+                            <p>Price: <span>car.price </span> </p>
+                            <p>Description: <span>car.description</span></p> <br/>
+
+                            <h5>VEHICLE DETAILS</h5>
+                            <hr/>
+                            <p>Make: <span> car.make </span> </p>
+                            <p>Model: <span> car.model </span> </p>
+                            <p>Year: <span> car.year </span> </p>
+                            <p>FuelType: <span> car.fueltype </span> </p>
+                            <p>Condition: <span> car.condition </span> </p>
+                            <p>Transmission: <span> car.transmission </span> </p>
+                            <p>Color: <span> car.color </span> </p>
+                        </Col>
+
+                        <Col md={7} >
+                            <div className="photo_details">
+                                <h5>Photo</h5>
+                                <br />
+                                <input type="file" id="img" name="img" accept="image/*" />
+                            </div>
+
+                            <div className="publish_details">
+                                <button className="first-section-btn">Publish</button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </React.Fragment>
+    );
+};
+export default ManageAds;
+
+
