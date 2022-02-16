c09cd7c49ec679f1389f10f6a2bd1e026a2e0b66
Lionsimba1994124 <lionsimba1994124@gmail.com>
2022-02-16 09:36:05 +0200

    integrating with model data

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -2,11 +2,18 @@ import React, {useState} from 'react';
 import "./Home.css";
 import {useNavigate} from "react-router-dom";
 import {Button} from "react-bootstrap";
-import Select from 'react-select'
+import Select from 'react-select';
+import Vehicles from '../../data/vehicles.json';
+import Makes from '../../data/makes.json';
+import Model_first_level from '../../data/model_first_levels.json';
 
 const Home = () => {
 
     const [advSearch, setAdvSearch] = useState(false)
+
+    const [vehicleValue, setVehicle] = useState('')
+    const [makeValue, setMake] = useState('')
+    const [modelFirstValue, setModelFirst] = useState('')
 
     const navigation = useNavigate();
     const changePageHandler = () => {
@@ -16,7 +23,7 @@ const Home = () => {
     let rowClass, toggleClass;
 
     if(advSearch){
-         rowClass = "col-md-4";
+        rowClass = "col-md-4";
         toggleClass = "show_search";
     }
     else{
@@ -24,10 +31,47 @@ const Home = () => {
         toggleClass = "hide_search";
     }
 
+    let vehicle_options = Vehicles.RECORDS.map(item => {       
+        return {
+            value: item.id,
+            label: item.vehicle_name
+        }
+    })
+
+    let makes_options = Makes.RECORDS.map(item => {
+            return {
+            value: item.id,
+            label: item.make_name,
+            parent_id: item.vehicle_id
+        }         
+    })
+
+    let model_first_level_options = Model_first_level.RECORDS.map(item => {
+        return {
+            value: item.id,
+            label: item.model_first_level_name,
+            parent_id: item.make_id
+        }
+    })
+
+    const setVehicleHandle = (value) => {
+        setVehicle(value)
+        setMake('')
+    } 
+
+    const setMakeHandle = (value ) => {         
+        setMake(value)
+        setModelFirst('')        
+    }   
+    
+    const setFirstModelHandle = (value) => {
+        setModelFirst(value)
+    }
+    
     const options = [
         { value: 'corolla', label: 'corolla' },
         { value: 'civic', label: 'civic' },
-    ]
+    ]   
 
     return (
         <React.Fragment>
@@ -50,13 +94,27 @@ const Home = () => {
                                     <div className="container">
                                     <div className="row home-select-div">
                                         <div className={rowClass}>
-                                            <Select placeholder={'Select Motors'} options={options} />
+                                            <Select 
+                                                placeholder = {'Select Motor'}
+                                                options={vehicle_options} 
+                                                onChange={(e) => setVehicleHandle(e)}
+                                                isSearchable={false}  />
                                         </div>
                                         <div className={rowClass}>
-                                            <Select placeholder={'Select Make'} options={options} />
+                                            <Select
+                                                placeholder = {'Select Makes'}
+                                                options={makes_options.filter(item => (item.value === "0" || (item.parent_id === vehicleValue.value )))}                                                 
+                                                onChange={(e) => setMakeHandle(e)} 
+                                                value= {makeValue}
+                                                isSearchable={false} />
                                         </div>
                                         <div className={rowClass}>
-                                            <Select placeholder={'Select Model'} options={options} />
+                                            <Select 
+                                                placeholder={'Select Model'} 
+                                                onChange={(e) => setFirstModelHandle(e)}
+                                                options={model_first_level_options.filter(item => (item.value === "0" || (item.parent_id === makeValue.value )))}
+                                                value= {modelFirstValue}
+                                                isSearchable={false} />
                                         </div>
                                         <div className={`${rowClass} ${toggleClass}`}>
                                             <Select placeholder={'Select Price'} options={options} />
