30c150e2d9a7d405727c0fe9ea919898ddd20f74
Lionsimba1994124 <lionsimba1994124@gmail.com>
2022-02-21 13:57:05 +0200

    dropdown options issue

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -22,6 +22,33 @@ export default function Dropdown(props) {
 
   const {user, setUser}  = useContext(UserContext);
 
+  const role = (Object.keys(user).length === 0 && user.constructor === Object) ? null : user.roles[0];  
+
+  const customerOptions = () => {
+    if(role === "CUSTOMER") {
+      return (        
+        <li>              
+          <NavLink to={'/new-ads'} onClick = {showDropdown}>NEW ADS</NavLink>
+        </li>    
+      )
+    }
+  }
+
+  const adminOptions = () => {
+    if(role === "ADMIN" || role === "MODERATOR") {
+      return (
+        <>
+          <li>              
+            <NavLink to={'/manage-ads'} onClick = {showDropdown}>MANAGE ADS</NavLink>
+          </li>
+          <li>              
+            <NavLink to={'/monitor-page'} onClick = {showDropdown}>MONITOR PAGE</NavLink>
+          </li>
+        </>
+      )
+    }
+  }
+
   return (
     <div className="container">
       <div className="menu-container">
@@ -36,9 +63,8 @@ export default function Dropdown(props) {
             <li>              
               <NavLink to={'/user-profile'} onClick = {showDropdown}>MANAGE PROFILE</NavLink>
             </li>
-            <li>              
-              <NavLink to={'/manage-ads'} onClick = {showDropdown}>MANAGE ADS</NavLink>
-            </li>   
+            {customerOptions()}
+            {adminOptions()}           
             <li>              
               <NavLink to={'/'} onClick = {logoutHalde}>LOGOUT</NavLink>
             </li>          
