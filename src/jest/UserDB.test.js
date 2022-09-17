594a87862893116101acc0aaf9bf3a1905305cdc
yudhbir <yudhbirsingh3066@gmail.com>
2022-09-17 20:54:41 +0530

    update the user save functionality and setup the user enum file

--- src/jest/UserDB.test.js
+++ src/jest/UserDB.test.js
@@ -1,9 +1,15 @@
 /* eslint-disable */
-import {userStatusChange} from "../repository/UserDB.js";
+import {userStatusChange,saveUser} from "../repository/UserDB.js";
+import User from "../models/User";
 
 test("Update the User Status: ", async () => {
     let userId="16bJFBu4qdiv4DB6Nsos";
     let status={status:true};
 	await expect(userStatusChange(userId,status)).resolves.toBe(true);
 
+});
+
+test("Create New User : ", async () => {	
+	let user=new User("devtest","Customer","909090900","test")
+	await expect(saveUser(user)).resolves.toBe(true);
 });
