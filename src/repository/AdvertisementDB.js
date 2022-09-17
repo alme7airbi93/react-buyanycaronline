4c1cb346254cb1a1594bd1ed10f25b2be0de7914
yudhbir <yudhbirsingh3066@gmail.com>
2022-09-17 07:09:58 +0530

    Setup the advertisement test case and improve the update function and remove demo test component

--- src/repository/AdvertisementDB.js
+++ src/repository/AdvertisementDB.js
@@ -3,31 +3,24 @@ import { db } from "./main";
 import Advertisement from "../models/Advertisement";
 const doc_collection = "advertisements";
 
-export const createAdvertisement = async(value=new Advertisement())=> {    
-    try {
-        let advt_data=JSON.parse(JSON.stringify(value));
-        const docRef = collection(db, doc_collection);
-        let update_doc = await addDoc(docRef,advt_data );
-        console.log(update_doc.id);
-    } catch (e) {
-        console.log("Error getting cached document:", e);
-    }
+export const createAdvertisement = async (value = new Advertisement()) => {
+	try {
+		let advt_data = JSON.parse(JSON.stringify(value));
+		const docRef = collection(db, doc_collection);
+		let update_doc = await addDoc(docRef, advt_data);
+		console.log(update_doc.id);
+		return true;
+	} catch (e) {
+		console.log("Error getting cached document:", e);
+	}
 };
-export const deActivateAdvertisement = async(advertId=null)=>{
-    try {        
-        const docRef = doc(db, doc_collection, advertId);
-        let update_doc = await updateDoc(docRef, {_status:0});
-        console.log(update_doc);
-    } catch (e) {
-        console.log("Error getting cached document:", e);
-    }
-};
-export const activateAdvertisement = async(advertId=null)=> {
-    try {        
-        const docRef = doc(db, doc_collection, advertId);
-        let update_doc = await updateDoc(docRef, {_status:1});
-        console.log(update_doc);
-    } catch (e) {
-        console.log("Error getting cached document:", e);
-    }
+export const advertisementStatusChange = async (advertId = null, status = {}) => {
+	try {
+		const docRef = doc(db, doc_collection, advertId);
+		await updateDoc(docRef, status);
+		//console.log(update_doc);
+		return true;
+	} catch (e) {
+		console.log("Error getting cached document:", e);
+	}
 };
