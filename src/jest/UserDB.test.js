25cfaf3da07e06dd5faa976f723f7e0b42f4c6c6
yudhbir <yudhbirsingh3066@gmail.com>
2022-09-17 22:42:17 +0530

    update the updatestatus function name into role

--- src/jest/UserDB.test.js
+++ src/jest/UserDB.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import {userStatusChange,saveUser,fetchUser} from "../repository/UserDB.js";
+import {changeUserRole,saveUser,fetchUser} from "../repository/UserDB.js";
 import User from "../models/User";
 
 test("Create New User : ", async () => {	
@@ -10,7 +10,7 @@ test("Create New User : ", async () => {
 test("Update the User Status: ", async () => {
     let userId="5kBvi6HBr8I6CH4hXeFj";
     let user=new User("devtest","Admin","909090900","test");
-	await expect(userStatusChange(userId,user)).resolves.toBe(true);
+	await expect(changeUserRole(userId,user)).resolves.toBe(true);
 
 });
 
