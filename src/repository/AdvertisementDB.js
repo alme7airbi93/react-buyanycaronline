7d68e42f21fa7aceb3f094c89bf796c39defa780
yudhbir <yudhbirsingh3066@gmail.com>
2022-09-21 13:15:37 +0530

    single record fetch implementation setup with make variable dynamic

--- src/repository/AdvertisementDB.js
+++ src/repository/AdvertisementDB.js
@@ -1,4 +1,4 @@
-import { doc, addDoc, collection, updateDoc, query, where, getDocs } from "firebase/firestore";
+import { doc, addDoc, getDoc ,collection, updateDoc, query, where, getDocs } from "firebase/firestore";
 import { db } from "./main";
 import Advertisement from "../models/Advertisement";
 const doc_collection = "advertisements";
@@ -9,7 +9,8 @@ export const createAdvertisement = async (value = new Advertisement()) => {
 		const docRef = collection(db, doc_collection);
 		let update_doc = await addDoc(docRef, advt_data);
 		console.log(update_doc.id);
-		return true;
+		return {success:true,advert_id:update_doc.id};
+		//return true;
 	} catch (e) {
 		console.log("Error getting cached document:", e);
 	}
@@ -22,6 +23,16 @@ export const advertisementStatusChange = async (advertId = null, value = new Adv
 		return true;
 	} catch (e) {
 		console.log("Error getting cached document:", e);
+	}
+};
+export const fetchSingleAdvertisement = async (advertId = null) => {
+	const docRef = doc(db, doc_collection, advertId);
+	const docSnap = await getDoc(docRef);
+	if (docSnap.exists()) {
+		console.log("Document data:", docSnap.data());
+		return {success:true,data:docSnap.data()};
+	} else {
+		return {success:false,data:[]};
 	}
 };
 export const fetchAdvertisement = async (value = new Advertisement()) => {
