596b70349fdd2431ba6f9edab3cc828ee8ac6690
craftandcode <craftandcode01@gmail.com>
2022-10-17 15:55:55 +0530

    trying to set fuel selection

--- src/common/data/SelectOptions.js
+++ src/common/data/SelectOptions.js
@@ -1,37 +1,44 @@
-import {Advertisement_Types} from "./Advertisement_Types.js";
-import {Boat_Types} from "./Boat_Types";
-import {Heavy_Vehicle_Types} from "./Heavy_Vehicle_Types";
-import {Motorcycle_Types} from "./Motorcycle_Types";
+import { Advertisement_Types } from "./Advertisement_Types.js";
+import { Boat_Types } from "./Boat_Types";
+import { Heavy_Vehicle_Types } from "./Heavy_Vehicle_Types";
+import { Motorcycle_Types } from "./Motorcycle_Types";
+import { Fuel_Types } from "./Fuel_Types";
 
-
-export const AdvertisementOptions = ()=>{
-	let options = [];
-	for (const [key, val] of Object.entries(Advertisement_Types)) {
-		options.push({value: key, label: val});
-	}
-	return options;
+export const AdvertisementOptions = () => {
+  let options = [];
+  for (const [key, val] of Object.entries(Advertisement_Types)) {
+    options.push({ value: key, label: val });
+  }
+  return options;
 };
 
-export const BoatOptions = ()=>{
-	let options = [];
-	for (const [key, val] of Object.entries(Boat_Types)) {
-		options.push({value: key, label: val});
-	}
-	return options;
+export const BoatOptions = () => {
+  let options = [];
+  for (const [key, val] of Object.entries(Boat_Types)) {
+    options.push({ value: key, label: val });
+  }
+  return options;
 };
 
-export const MotorcycleOptions = ()=>{
-	let options = [];
-	for (const [key, val] of Object.entries(Motorcycle_Types)) {
-		options.push({value: key, label: val});
-	}
-	return options;
+export const MotorcycleOptions = () => {
+  let options = [];
+  for (const [key, val] of Object.entries(Motorcycle_Types)) {
+    options.push({ value: key, label: val });
+  }
+  return options;
 };
 
-export const HeavyVehicleOptions = ()=>{
-	let options = [];
-	for (const [key, val] of Object.entries(Heavy_Vehicle_Types)) {
-		options.push({value: key, label: val});
-	}
-	return options;
+export const HeavyVehicleOptions = () => {
+  let options = [];
+  for (const [key, val] of Object.entries(Heavy_Vehicle_Types)) {
+    options.push({ value: key, label: val });
+  }
+  return options;
+};
+export const FuelTypes = () => {
+  let options = [];
+  for (const [key, val] of Object.entries(Fuel_Types)) {
+    options.push({ value: key, label: val });
+  }
+  return options;
 };
