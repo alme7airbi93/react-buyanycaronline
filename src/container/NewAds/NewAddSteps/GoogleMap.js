2bd21757223bf6a47db157dafbdf084cea120000
Abhilash Sharma <abhilashsharma@Abhilashs-Mac-mini.local>
2022-10-19 17:02:22 +0530

    add photo after data save

--- src/container/NewAds/NewAddSteps/GoogleMap.js
+++ src/container/NewAds/NewAddSteps/GoogleMap.js
@@ -8,23 +8,69 @@ import { AdvertismentCtx } from "../../../context/AdvertismentContext.js";
 
 import "./scrollbar.css";
 import { createAdvertisement } from "../../../common/repository/AdvertisementDB";
+import {
+	getDownloadURL,
+	uploadBytes,
+	getStorage,
+	listAll,
+	ref,
+	uploadBytesResumable,
+  } from 'firebase/storage'
+
 
 const GoogleMap = (props) => {
   
 	const adsCtx =  useContext(AdvertismentCtx)
 	const advertisement = adsCtx.ads;
+	const [photos,setPhotos] = useState()
 
 	const center = { lat: 24.4539, lng: 54.3773 };
 	const zoom = 4;
 	
 	useEffect(()=>{
+		setPhotos(advertisement.photos)
 		console.log(advertisement,'in google')
 	},[])
 
 
+	const uploadImageNow = async (file,id,filename) => {
+		console.log(file,id,filename+'.png','file')
+		const imagePath = 'ads/'+id;
+		if (!file) {
+		  alert('Image cannnot be blank')
+		  return
+		}
+		const storage = getStorage()
+		const storageRef = ref(storage, imagePath + `/${filename}.png`)
+		const task = await uploadBytes(storageRef, file).then((res) =>
+		  console.log(res.ref),
+		)
+		const url = await getDownloadURL(storageRef)
+		console.log(url,'url')
+		// await uploadEventImage(eventId, url, imageType)
+		// toast('image uploaded successfully')
+		// setLoading(false)
+		// refreshData(true)
+	  }
+
+
+	const savePhotos = (id) => {
+		var filename = 1;
+		photos.forEach((data)=> {
+			filename = filename + 1;
+			uploadImageNow(data,id,filename)
+		})
+	}
+
 	const saveData = () =>{
+		var dt = advertisement;
+		dt.photos = [];
+		console.log(dt,'save data')
+		console.log(photos,'photos')
 		createAdvertisement(advertisement).then((res)=>{
 			if(res.success){
+				console.log(res.data,res)
+				savePhotos(res.data)
 				alert('Data uploaded successfully')
 
 			}
